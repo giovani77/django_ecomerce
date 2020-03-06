1ec0e4f4a4d9e20ed88491720c05c34f70ea9d73
Giovani de Oliveira <giovani77@gmail.com>
2020-03-06 00:25:14 -0300

    arrumado a view

--- lojavirtual/views.py
+++ lojavirtual/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Página principal",
+        "content": "Seja bem-vindo a página principal",
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Página sobre",
+        "content": "Seja bem-vindo a página sobre",
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Página contato",
+        "content": "Seja bem-vindo a página contato",
+    }
+    return render(request, "contact/view.html", context)
