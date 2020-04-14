e2cbce899ffcb3fd034633dd167ff1e3831d2d67
Giovani de Oliveira <giovani77@gmail.com>
2020-04-13 23:03:01 -0300

    criado modelo de produto

---
+++ produtos/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Produto(models.Model): #produto_categoria
+    nome        = models.CharField(max_length=120)
+    descricao   = models.TextField()
+    preco       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
