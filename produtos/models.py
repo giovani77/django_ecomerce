d7e1aaff572c25949878931543bacc8b2eaa64d9
Giovani de Oliveira <giovani77@gmail.com>
2020-04-13 23:28:45 -0300

    colocando o nome do produto para mostrar na tela de admin

--- produtos/models.py
+++ produtos/models.py
@@ -5,3 +5,7 @@ class Produto(models.Model): #produto_categoria
     nome        = models.CharField(max_length=120)
     descricao   = models.TextField()
     preco       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+
+    #pythen 3
+    def __str__(self):
+        return self.nome
