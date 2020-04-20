46f62ba84c326d583c27a735e40cba5b38b159b3
Giovani de Oliveira <giovani77@gmail.com>
2020-04-20 00:39:55 -0300

    trabalhando com a lista

--- produtos/views.py
+++ produtos/views.py
@@ -8,12 +8,17 @@ class ProdutoListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada
     queryset = Produto.objects.all()
     template_name = "produtos/list.html"
+    
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProdutoListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
 
 #Function Based View
 def produto_list_view(request):
     queryset = Produto.objects.all()
     context = {
-        'qs': queryset
+        'object_list': queryset
     }
     return render(request, "produtos/list.html", context)
 
