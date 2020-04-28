56cd44bdc369042b3c1f9a089156c0dfc6f16772
Giovani de Oliveira <giovani77@gmail.com>
2020-04-27 21:24:52 -0300

    modificando tudo

--- produtos/models.py
+++ produtos/models.py
@@ -5,7 +5,12 @@ class Produto(models.Model): #produto_categoria
     nome        = models.CharField(max_length=120)
     descricao   = models.TextField()
     preco       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    imagem      = models.ImageField(upload_to = 'produtos/', null=True, blank=True)
 
     #pythen 3
     def __str__(self):
+        return self.nome
+
+    #python 2
+    def __unicode__(self):
         return self.nome
