9a4b887b6e24bc62aa1badadfb432757b7ec75d5
Giovani de Oliveira <giovani77@gmail.com>
2020-05-01 00:14:15 -0300

    criada as classes ProdutoQuerySet, adicionados metodos ehProdutoAtivo, ehDestaque e adicionado os atributos a classe Produto

--- src/produtos/models.py
+++ src/produtos/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
+#Custom queryset
+class ProdutoQuerySet(models.query.QuerySet):
+    def ehProdutoAtivo(self):
+        return self.filter(ehProdutoAtivo = True)
+
+    def ehDestaque(self):
+        return self.filter(ehDestaque = True, ehProdutoAtivo = True)
+
 class ProdutoManager(models.Manager):
+    def get_queryset(self):
+        return ProdutoQuerySet(self.model, using = self._db)
+    
+    def all(self):
+        return self.get_queryset().ehProdutoAtivo()
+
+    def ehDestaque(self):
+        #return self.get_queryset().filter(ehDestaque = True)
+        return self.get_queryset().ehDestaque()
+    
     def get_by_id(self, id):
         qs = self.get_queryset().filter(id = id)
         if qs.count() == 1:
@@ -9,10 +27,12 @@ class ProdutoManager(models.Manager):
 
 # Create your models here.
 class Produto(models.Model): #produto_categoria
-    nome        = models.CharField(max_length=120)
-    descricao   = models.TextField()
-    preco       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
-    imagem      = models.ImageField(upload_to = 'produtos/', null=True, blank=True)
+    nome            = models.CharField(max_length=120)
+    descricao       = models.TextField()
+    preco           = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    imagem          = models.ImageField(upload_to = 'produtos/', null=True, blank=True)
+    ehDestaque      = models.BooleanField(default = False)
+    ehProdutoAtivo  = models.BooleanField(default = True)
 
     objects = ProdutoManager()
     
