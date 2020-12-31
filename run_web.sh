f63dc5b0fedf9ef8459bc1f0c705bb8a286423ca
Giovani Ricardo de Oliveira <giovani77@gmail.com>
2020-12-31 03:33:42 -0300

    Arrumado arquivo

--- run_web.sh
+++ run_web.sh
@@ -12,19 +12,19 @@ pip install --upgrade pip
 #Django não suporta servir arquivos estáticos na produção. No entanto, o fantástico projeto WhiteNoise pode ser integrado ao seu aplicativo Django e foi projetado exatamente com esse objetivo em mente. Veja a documentação do WhiteNoise para mais detalhes.
 pip install django whitenoise
 
-#migrando o banco de dados.
-python src/manage.py migrate
-
-#Acessar diretório do projeto
-cd src
-
 python -m pip install Pillow
 
 #Isso irá copiar todos os arquivos dos seus diretórios estáticos para dentro do diretório definido em STATIC_ROOT.
-python manage.py collectstatic --noinput
+python src/manage.py collectstatic --noinput
+
+#migrando o banco de dados.
+python src/manage.py migrate
 
 #criando o super usuário do para administração via web.
-python manage.py createsuperuser
+python src/manage.py createsuperuser
+
+#Acessar diretório do projeto
+cd src
 
 #rodando servidor web na porta 8000.
 python manage.py runserver 0.0.0.0:8000
